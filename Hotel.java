660488b5db9a2b7f4230aeeef09011e943e050e0
Gururaj <bevinamaradgururaj@gmail.com>
2024-02-10 11:36:50 +0530

    upadated

--- Hotel.java
+++ Hotel.java
@@ -1,10 +1,11 @@
-import java.util.Scanner;
+ import java.util.Scanner;
 
-public class Hotel
+public class Hotel extends branch
  {
     public static void main(String[] args)
      {
         Scanner scanner = new Scanner(System.in);
+        branch ne=new branch();
         System.out.print("Enter customer name: ");
         String customerName = scanner.nextLine();
 
@@ -13,11 +14,12 @@ public class Hotel
 
         System.out.print("Enter assigned room number: ");
         int assignedRoomNo = scanner.nextInt();
-        
+       
         System.out.println("\nCustomer Information:");
         System.out.println("Name: " + customerName);
         System.out.println("Date of Booking: " + dateOfBooking);
-        System.out.println("Assigned Room Number: " + assignedRoomNo);
+        //System.out.println("Assigned Room Number: " + assignedRoomNo);
+        floor(assignedRoomNo);
         scanner.close();
     }
 }
---
+++ branch.java
@@ -0,0 +1,15 @@
+public class branch {
+    public static void floor(int as)
+    {
+        if(as==101)
+        {
+            System.out.println("1st");
+        }
+        else if(as==102){
+            System.out.println("2nd");
+        }
+        else if (as==103){
+            System.out.println("3rd");
+        }
+    }
+}
